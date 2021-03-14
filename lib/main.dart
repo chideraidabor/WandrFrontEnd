927b914affab6b3b83f795c120e259df58388329
admin <Gannon@admins-Mac-mini.local>
2021-03-14 01:14:01 -0500

    add sign in, sign up screens, navigations, and a welcome screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,23 @@
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
-import 'package:wandr_frontend/views/home.dart';
+import 'Screens/Welcome/welcome_screen.dart';
+import 'constants.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Auth',
       theme: ThemeData(
-        primarySwatch: Colors.red,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
+        primaryColor: kPrimaryColor,
+        scaffoldBackgroundColor: Colors.white,
       ),
-      home: searchEvent(),
+      home: WelcomeScreen(),
     );
   }
 }
+
 
--- lib/views/tabs/chats.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter/material.dart';
-class chatPage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Container();
-  }
-}
